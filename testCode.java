c1cb4a290e15c4031f731919f6aa8e6cbc66b50e
kday92 <kday@bstonetech.com>
2015-08-05 14:50:42 -0400

    Update testCode.java
    Testing to see if errors are thrown inside of github when syntax errors are introduced

--- testCode.java
+++ testCode.java
@@ -1,5 +1,6 @@
 public class testCode {
 public static void main (String [] args) {
 System.out.println("Testing the repository on github");
+String k = 2.2+2; //Check to see if git gives me errors on mistakes inside of the code
   }
 }
